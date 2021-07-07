9cbb5104c9042f688ef09030d41f167fb1b480f5
Odedoyin Akindele <akindeleodedoyin@gmail.com>
2021-07-07 19:10:03 +0100

    https://leetcode.com/problems/peak-index-in-a-mountain-array/ edited

--- src/main/java/easy2/PeakIndexInMountainArray.java
+++ src/main/java/easy2/PeakIndexInMountainArray.java
@@ -2,6 +2,7 @@ package easy2;
 
 public class PeakIndexInMountainArray {
     public static void main(String[] args) {
+        //https://leetcode.com/problems/peak-index-in-a-mountain-array/
         System.out.println(peakIndexInMountainArray(new int[]{24,69,100,99,79,78,67,36,26,19}));
         System.out.println(peakIndexInMountainArray(new int[]{0, 1, 0}));
         System.out.println(peakIndexInMountainArray(new int[]{0,2, 1, 0}));
