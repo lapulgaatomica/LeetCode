c117a84d6a319b06b97a5c796eac9c4704679c49
Odedoyin Akindele <akindeleodedoyin@gmail.com>
2021-08-25 10:08:16 +0100

    https://leetcode.com/problems/merge-two-sorted-lists/

---
+++ src/main/java/customdatastructures/ListNodeTest.java
@@ -0,0 +1,15 @@
+package customdatastructures;
+
+public class ListNodeTest {
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+
+        System.out.println(n1);
+    }
+}
---
+++ src/main/java/easy1/MergeTwoLists.java
@@ -0,0 +1,45 @@
+package easy1;
+
+import customdatastructures.ListNode;
+
+public class MergeTwoLists {
+    //https://leetcode.com/problems/merge-two-sorted-lists/
+    public static void main(String[] args) {
+        ListNode firstList = new ListNode(1);
+        firstList.next = new ListNode(2);
+        firstList.next.next = new ListNode(4);
+
+        ListNode secondList = new ListNode(1);
+        secondList.next = new ListNode(3);
+        secondList.next.next = new ListNode(4);
+
+        ListNode merged = mergeTwoLists(firstList, secondList);
+        System.out.println(merged);
+    }
+
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2){
+        // Problem wasn't solved by me
+        ListNode head = new ListNode(0);
+        ListNode last = head;
+
+        while(l1 != null && l2 != null) {
+            if(l1.val > l2.val) {
+                last.next = l2;
+                l2 = l2.next;
+            }else{
+                last.next = l1;
+                l1 = l1.next;
+            }
+            last = last.next;
+        }
+
+        if(l1 == null) {
+            last.next = l2;
+        } else {
+            last.next = l1;
+        }
+
+        return head.next;
+    }
+}
+
