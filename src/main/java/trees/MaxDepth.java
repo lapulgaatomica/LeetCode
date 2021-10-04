b891bd5905d385c62b5a6e3e26928cd904be2d4b
Odedoyin Akindele <akindeleodedoyin@gmail.com>
2021-10-04 20:39:20 +0100

    https://leetcode.com/problems/maximum-depth-of-n-ary-tree/

---
+++ src/main/java/trees/MaxDepth.java
@@ -0,0 +1,36 @@
+package trees;
+
+import java.util.List;
+
+public class MaxDepth {
+    //https://leetcode.com/problems/maximum-depth-of-n-ary-tree/
+
+    public static int maxDepth(Node root) {
+        int maxDepth = 0;
+        if(root == null){
+            return 0;
+        }
+
+        for(Node child : root.children){
+            maxDepth = Math.max(maxDepth, maxDepth(child));
+        }
+
+        return maxDepth + 1;
+    }
+}
+
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
